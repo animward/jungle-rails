fb2224c2f6ffe6f22ea856a38d53d395595a8048
animward <animward@gmail.com>
2024-05-07 10:46:41 -0700

    tests for product details

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -12,8 +12,11 @@ describe('jungle store', () => {
     it('There is products on the page', () => {
       cy.get('.products article').should('be.visible');
     });
-    it('There is 2 products on the page', () => {
-      cy.get('.products article').should('have.length', 2);
+    describe('clicking on product', () => {
+      it('displays the product page for that product', () => {
+        cy.get('.products article').first().click();
+        cy.get('.product-detail').should('be.visible');
+      })
     });
   });
 });
